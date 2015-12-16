363a95ffa3ea596afd4cbf28c26bb06a55d306f5
Torin Blankensmith <torin.blankensmith@gmail.com>
2015-12-15 19:34:16 -0800

    Fixed background color on Song Picker

--- PitchPerfect/SongListTableViewController.swift
+++ PitchPerfect/SongListTableViewController.swift
@@ -19,7 +19,7 @@ class SongListTableViewController: UITableViewController, SongsDelegate {
         
         // hides empty cells
         self.tableView.tableFooterView = UIView();
-        
+        self.tableView.backgroundColor = UIColor.whiteColor()
         // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
         // self.navigationItem.rightBarButtonItem = self.editButtonItem()
     }
